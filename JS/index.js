607d31e0f8fb1acfc45cce94383c324868be2b7d
fardin-alve <fialve2809@gmail.com>
2024-09-24 00:19:34 +0600

    without history

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,37 @@
+const cardContainer = document.getElementById('cards-container')
+const historyContainer = document.getElementById('history-container')
+const historyChild = document.getElementById('history-child')
+const historyButton = document.getElementById('history-button');
+const donationBtn = document.getElementById('donation-btn');
+const blogButton = document.getElementById('blog-button')
+const navBar = document.getElementById('nav-section')
+
+window.addEventListener('scroll', function () {
+    navBar.classList.add('backdrop-blur-lg')
+})
+blogButton.addEventListener('click', function () {
+    window.location.assign('blog.html');
+})
 document.getElementById('first-donate-button').addEventListener('click', function () {
     const availableBalance = parseFloat(document.getElementById('main-balance').innerText)
-    if (!isNaN(donateButtonId('first-input')) && donateButtonId('first-input') < availableBalance) {
+    if (!isNaN(donateButtonId('first-input')) && donateButtonId('first-input') < availableBalance && donateButtonId('first-input') >= 0) {
         const moneyAmount = donateButtonId('first-input') + cardMoneyId('first-card-money')
+        document.getElementById('my_modal_5').showModal()
         document.getElementById('first-card-money').innerText = moneyAmount;
         const available = availableBalance - donateButtonId('first-input');
         document.getElementById('main-balance').innerText = available;
+        
     }
     else {
         alert('Invalid Amount')
     }
-    
+
 })
 document.getElementById('second-donate-button').addEventListener('click', function () {
     const availableBalance = parseFloat(document.getElementById('main-balance').innerText);
     if (!isNaN(donateButtonId('second-input')) && donateButtonId('second-input') < availableBalance) {
         const moneyAmount = donateButtonId('second-input') + cardMoneyId('second-card-money')
+        document.getElementById('my_modal_5').showModal()
         document.getElementById('second-card-money').innerText = moneyAmount;
         const available = availableBalance - donateButtonId('second-input');
         document.getElementById('main-balance').innerText = available;
@@ -25,8 +42,9 @@ document.getElementById('second-donate-button').addEventListener('click', functi
 })
 document.getElementById('third-donate-button').addEventListener('click', function () {
     const availableBalance = parseFloat(document.getElementById('main-balance').innerText)
-    if (!isNaN(donateButtonId('third-input')) && donateButtonId('third-input') < availableBalance ) {
+    if (!isNaN(donateButtonId('third-input')) && donateButtonId('third-input') < availableBalance) {
         const moneyAmount = donateButtonId('third-input') + cardMoneyId('third-card-money')
+        document.getElementById('my_modal_5').showModal()
         document.getElementById('third-card-money').innerText = moneyAmount;
         const available = availableBalance - donateButtonId('third-input');
         document.getElementById('main-balance').innerText = available;
@@ -34,4 +52,21 @@ document.getElementById('third-donate-button').addEventListener('click', functio
     else {
         alert('Invalid Amount')
     }
+
 })
+donationBtn.addEventListener('click', function () {
+    donationBtn.classList.add('bg-btn_color', 'text-black');
+    historyButton.classList.remove('bg-btn_color', 'text-black');
+    historyButton.classList.add('text-gray-500', 'font-medium', 'border');
+    cardContainer.classList.remove('hidden')
+})
+historyButton.addEventListener('click', function () {
+    historyButton.classList.add('bg-btn_color', 'text-black');
+    donationBtn.classList.remove('bg-btn_color', 'text-black');
+    donationBtn.classList.add('text-gray-500', 'font-medium', 'border');
+    cardContainer.classList.add('hidden');
+    historyContainer.classList.remove('hidden');
+})
+
+  
+
--- JS/utilities.js
+++ JS/utilities.js
@@ -6,3 +6,7 @@ function cardMoneyId(id) {
     const cardMoney = parseFloat(document.getElementById(id).innerText)
     return cardMoney;
 }
+function qoutesbyId(id) {
+    const qoute = document.getElementById(id).innerText;
+    return qoute;
+}
